fc81ede594f3485bd804c262b83af4181d798874
LeviNicolaDev <levinicolas2000@gmail.com>
2025-09-27 16:54:22 -0300

    [feat] configuração do modelo de Veiculos

--- Infrastructure/Db/DbContexto.cs
+++ Infrastructure/Db/DbContexto.cs
@@ -13,6 +13,7 @@ public class DbContexto : Microsoft.EntityFrameworkCore.DbContext
     }
     
     public DbSet<Administrator> Administrators { get; set; } = default!;
+    public DbSet<Vehicle> Vehicles { get; set; } = default!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
