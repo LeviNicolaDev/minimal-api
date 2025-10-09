6e4e604340d53a0578b6593f55c2c16a423a20e8
LeviNicolaDev <levinicolas2000@gmail.com>
2025-10-09 19:54:27 -0300

    [chore] adicionados arquivos não-versionados pelo git

---
+++ Api/Domain/ModelViews/Home.cs
@@ -0,0 +1,13 @@
+﻿namespace minimal_api.Domain.ModelViews;
+
+public struct Home
+{
+    public string Mensagem
+    {
+        get => "Bem vindo(a) a API de veículos - Minimal API";
+    }
+    public string Doc
+    {
+        get => "/swagger";
+    }
+}
